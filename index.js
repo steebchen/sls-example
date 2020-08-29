10d049bc8401da50255eeaa422cb819ad6c51407
Luca Steeb <contact@luca-steeb.com>
2020-08-30 00:31:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { v4: uuid } = require('uuid')
 
 module.exports = (req, res) => {
-	res.end('omg, it actually works!! here\'s a uuid: ' + uuid())
+	res.end('hi! here\'s a uuid: ' + uuid())
 }
